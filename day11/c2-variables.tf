18266ca4ca83ea1de46f14fb68ec32f96107ce45
Kalpesh Kumar <kalpesh.kumar@einfochips.com>
2026-05-19 05:21:51 +0000

    updated readme.md along with helm code

---
+++ day11/c2-variables.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment_name" {
+  description = "Environment name used in resource names and tags"
+  type        = string
+  default     = "dev-kalpesh"
+}
+
+
+variable "tags" {
+  description = "Global tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Terraform = "true"
+  }
+}
+
---
+++ day11/c4-outputs.tf
@@ -0,0 +1,27 @@
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "VPC ID used by EKS and other services"
+}
+
+output "private_subnet_ids" {
+  value       = module.vpc.private_subnet_ids
+  description = "Private subnets for EKS worker nodes"
+}
+
+
+output "public_subnet_ids" {
+  value       = module.vpc.public_subnet_ids
+  description = "Public subnets for ALB, NLB, etc."
+}
+
+output "public_subnet_map" {
+  value       = module.vpc.public_subnet_map
+  description = "Public subnets for ALB, NLB, etc."
+}
+
+output "internet_gateway_id" {
+  value       = module.vpc.internet_gateway_id
+  description = "Internet Gateway ID attached to the VPC"
+}
+
+
